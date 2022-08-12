10e0415c3dc7d19579a3226a0daeb31ff3b53b22
Lara Namiq <larasnamiq@gmail.com>
2022-08-12 14:31:49 +0100

    Add solution

--- app/index.test.js
+++ app/index.test.js
@@ -1,11 +1,25 @@
-import { tryConvert } from "./index.js";
+import tryConvert from "./index.js";
 
-test("should convert 100 celsius to 212 fahrenheit", () => {
-  const tempToFah = tryConvert(100, 212);
-  expect(tempToFah((100 * 9) / 5 + 32)).toBe("212");
+it("converts 100 celsius to 212 fahrenheit", () => {
+  // Arrange
+  const temp2Convert = 100;
+  const conversion = "toFahrenheit";
+
+  // Act
+  const result = tryConvert(temp2Convert, conversion);
+
+  // Assert
+  expect(result).toBe("212");
 });
 
-test("should convert 50 fahrenheit to 10 celsius", () => {
-  const tempToCel = tryConvert(50, 10);
-  expect(tempToCel((tryConvert - 32) * 5) / 9).toBe(10);
+it("converts 50 fahrenheit to 10 celsius", () => {
+  // Arrange
+  const temp2Convert2 = 50;
+  const conversion2 = "toCelsius";
+
+  // Act
+  const result2 = tryConvert(temp2Convert2, conversion2);
+
+  // Assert
+  expect(result2).toBe("10");
 });
