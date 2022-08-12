072e80df0be26d5d42f7da63642a15781255ea1c
Lara Namiq <larasnamiq@gmail.com>
2022-08-12 13:53:35 +0100

    Add two tests

--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,11 @@
-import { add2Nums } from "./index.js";
+import { tryConvert } from "./index.js";
 
-test("1 + 1 = 2", () => {
-  expect(add2Nums(1, 1)).toBe(2);
+test("should convert 100 celsius to 212 fahrenheit", () => {
+  const tempToFah = tryConvert(100, 212);
+  expect(tempToFah((100 * 9) / 5 + 32)).toBe("212");
+});
+
+test("should convert 50 fahrenheit to 10 celsius", () => {
+  const tempToCel = tryConvert(50, 10);
+  expect(tempToCel((tryConvert - 32) * 5) / 9).toBe(10);
 });
